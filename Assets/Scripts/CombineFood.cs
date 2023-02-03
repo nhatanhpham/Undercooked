46c42c6dc5e15dbd887b3c5d4d5e93c45cef3b6e
KaiserElite <erik.le.279@gmail.com>
2023-02-02 22:50:36 -0800

    FoodCombine (all code currently not able to run)
    Adding FoodCombine Singleton (although it's bare bones, nothing useable as of right now)

---
+++ Assets/Scripts/CombineFood.cs
@@ -0,0 +1,18 @@
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombineFood : Singleton<CombineFood>
+{
+
+    //Push a list of ingredient names to this function in order to match the ingredients to new dishes
+    public Card cookFood(List<string> ingredients)
+    {
+        List<string> steakAndEggs = new List<string> { "Steak", "Eggs" };
+        if (ingredients.Equals(steakAndEggs))
+        {
+
+        }
+    }
+}
+*/
